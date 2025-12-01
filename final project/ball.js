477db1f5cb077757fee9f0719c11dafb9b98929e
carol1newang <cw4265@nyu.edu>
2025-12-01 15:50:54 -0500

    2nd push

--- final project/ball.js
+++ final project/ball.js
@@ -1,12 +1,69 @@
 class ball{
-  constructor(x, y, size){
+  constructor(x, y, size, r, g, b){
     this.x = x;
     this.y = y;
     this.size = size;
+    this.r = r;
+    this.g = g;
+    this.b = b;
+
+    if(this.r == 255){
+      this.colorIsRed = true;
+    }
+    if(this.b == 255){
+      this.colorIsBlue = true;
+    }
+    if(this.g == 255){
+      this.colorIsGreen = true;
+    }
   }
 
-  display(r, g, b){
+  display(){
+    fill(this.r, this.g, this.b);
+    ellipse(this.x, this.y, this.size);
+  }
+
+  main(r, g, b){
     fill(r, g, b);
     ellipse(this.x, this.y, this.size);
+  }
+
+  drop(speed){
+    if(this.y < height - 30){
+      this.y+=speed;
+    }
+    else if(this.x > mouseX-20 && this.x < mouseX+30){
+      if(this.colorIsRed){
+        r += 85;
+        this.x=random(0, width);
+        this.y=0;
+      }
+      else if(this.colorIsGreen){
+        g += 85;
+        this.x=random(0, width);
+        this.y=0;
+      }
+      else if(this.colorIsBlue){
+        b += 85;
+        this.x=random(0, width);
+        this.y=0;
+      }
+    }
+    else{
+      this.x=random(0, width);
+      this.y=0;
+    }
+  }
+
+  change(){
+    if (key == 'r'){
+      this.r = lerp(this.r, 255, 0.333);
+    }
+    if (key == 'g'){
+      this.g = lerp(this.g, 255, 0.333);
+    }
+    if (key == 'b'){
+      this.b = lerp(this.b, 255, 0.333);
+    }
   }
 }
--- final project/sketch.js
+++ final project/sketch.js
@@ -2,14 +2,39 @@ let r = 0;
 let b = 0;
 let g = 0;
 let mainBall;
+let redY = 0;
+let speed;
+let levelR, levelG, levelB
 
 function setup(){
 	createCanvas(600, 600);
-	background(255);
+	background(50, 50, 50);
+	noStroke();
 
-	mainBall = new ball(300, 300, 100);
+	mainBall = new ball(300, 300, 400);
+	redBall = new ball(random(0, width), 0, 50, 255, 0, 0);
+	greenBall = new ball(random(0, width), 0, 50, 0, 255, 0);
+	blueBall = new ball(random(0, width), 0, 50, 0, 0, 255);
+
+	levelR = random(0, 3)*255/random(1,5);
+	levelG = random(0, 3)*255/random(1,5);
+	levelB = random(0, 3)*255/random(1,5);
 }
 
 function draw(){
-	mainBall.display(r, g, b);
+	background(50, 50, 50);
+	bucket();
+	mainBall.main(levelR, levelG, levelB);
+	redBall.display();
+	redBall.drop(random(1, 3));
+	greenBall.display();
+	greenBall.drop(random(1, 3));
+	blueBall.display();
+	blueBall.drop(random(1, 3));
+}
+
+function bucket(){
+	rectMode(CENTER);
+	fill(r, g, b);
+	rect(mouseX, height-20, 80, 40);
 }
